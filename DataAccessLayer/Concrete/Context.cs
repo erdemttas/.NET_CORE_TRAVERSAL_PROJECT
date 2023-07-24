5cfec7d283fe16cffe4d4362f7de8438f804fc6f
Erdem Taş <erdemtas60@gmail.com>
2023-07-24 19:28:51 +0300

    PresentationLayer, DataAccesLayer, BusinessLayer, EntityLayer Katmanları Oluşturuldu.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+	public class Context:DbContext
+	{
+		// Veri tabanı bağlantı kurma metodu.
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=.\\SQLEXPRESS;database=TraversalDB;integrated security=true;");
+		}
+
+		// EntityLayer'daki sınıf isimleri - sql'e yansıyacak tablo isimleri
+		public DbSet<About> Abouts { get; set; }
+		public DbSet<About2> Abouts2s { get; set; }
+		public DbSet<Contact> Contacts { get; set; }
+		public DbSet<Destination> Destinations { get; set; }
+		public DbSet<Feature> Features { get; set; }
+		public DbSet<Feature2> Feature2s { get; set; }
+		public DbSet<Guide> Guides { get; set; }
+		public DbSet<NewsLetter> NewsLetters { get; set; }
+		public DbSet<SubAbout> SubAbouts { get; set; }
+		public DbSet<Testimonial> Testimonials { get; set; }
+	}
+}
---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+	public class GenericRepository<T> : IGenericDal<T> where T : class  //Gelen T değerleri bir sınıf olmalıdır koşulu.
+	{
+		public void Delete(T t)
+		{
+			Context c = new Context();
+			c.Remove(t);
+			c.SaveChanges();
+		}
+
+		public List<T> GetList()
+		{
+			Context c= new Context();
+			return c.Set<T>().ToList();
+		}
+
+		public void Insert(T t)
+		{
+			Context c= new Context();
+			c.Add(t);
+		}
+
+		public void Update(T t)
+		{
+			Context c = new Context();
+			c.Update(t);
+		}
+
+	}
+}
